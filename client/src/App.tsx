fac1a28047acb56afabbed42f9e36601809a985a
CédricGOUYON <gouyoncedric@gmail.com>
2025-07-16 20:24:18 +0200

    first project commit

---
+++ client/src/App.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router";
+import { ToastContainer } from "react-toastify";
+import Footer from "./components/footer/Footer";
+import Header from "./components/header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+      <ToastContainer />
+    </>
+  );
+}
+
+export default App;
---
+++ server/src/index.ts
@@ -0,0 +1,42 @@
+import cors from "cors";
+import dotenv from "dotenv";
+import express from "express";
+import mysql from "mysql2/promise";
+import routes from "../src/routes/api";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.APP_PORT || 3310;
+
+// Middleware
+app.use(cors({ origin: process.env.CLIENT_URL || "http://localhost:5173" }));
+app.use(express.json());
+
+// Routes API
+app.use("/api", routes);
+
+// Ping principal
+app.get("/", (_req, res) => {
+  res.send("Backend Express en marche ✅");
+});
+
+// Connexion MySQL + message terminal
+mysql
+  .createConnection({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  })
+  .then(() => {
+    console.log(`🗄️ using database ${process.env.DB_NAME}`);
+  })
+  .catch((err) => {
+    console.error("❌ Erreur de connexion MySQL :", err);
+  });
+
+// Lancement du serveur
+app.listen(PORT, () => {
+  console.log(`🚀 Serveur lancé sur http://localhost:${PORT}`);
+});
